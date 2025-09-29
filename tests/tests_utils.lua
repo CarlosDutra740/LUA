51460b2488b8e4064b4e353aaaa61dbb3c3e879b
CarlosDutra740 <amorimdutra.carlos@gmail.com>
2025-09-29 09:05:19 -0300

    Create tests_utils.lua

---
+++ tests/tests_utils.lua
@@ -0,0 +1,9 @@
+-- test_utils.lua
+-- Testes simples para utils.lua
+
+local utils = require("src.utils")
+
+print("Executando testes de utils...")
+
+utils.say_hello("Teste")
+-- Aqui você poderia integrar frameworks de teste como 'busted'
